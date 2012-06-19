734113a16a2224948999ee1cb2c0f516f3686668
speedydaniel <Speedy.daniel77@gmail.com>
2012-06-19 03:41:46 +0300

    Update master

--- src/com/cmrabbitfootTUN_SD/pYrplerabbYt/Foo.java
+++ src/com/cmrabbitfootTUN_SD/pYrplerabbYt/Foo.java
@@ -7,6 +7,6 @@ public class Foo extends Activity
 }
 
 /* Location:           C:\Users\speedy\Desktop\NEWapk\jd-gui-0.3.3.windows\classes_dex2jar.jar
- * Qualified Name:     com.cmjatx2wne.simpleswagger.Foo
+ * Qualified Name:     com.rabbitfootTUN_SD.pYrplerabbYt.Foo
  * JD-Core Version:    0.6.0
  */
